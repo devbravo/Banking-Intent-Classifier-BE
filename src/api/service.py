91474493c02f23f1192a53708b13bfcdbfc22b99
Diego Sabajo <diegofranco711@gmail.com>
2024-09-03 18:20:30 -0300

    integration with feedback table now working

--- src/api/service.py
+++ src/api/service.py
@@ -20,12 +20,12 @@ import json
 import torch
 import torch.nn.functional as F
 import bentoml
-from bentoml.io import Text
+from bentoml.io import Text, JSON
 from src.utils.label_mapping import label_mapping
 from src.data_preprocessing.text_processing import clean_text, lemmatizer
 from src.data_preprocessing.text_processing import numericalize
 from src.utils.get_device import get_device
-from src.api.database import log_query_to_db
+from src.api.database import log_query_to_db, log_feedback_to_db
 
 
 DEVICE = get_device()
@@ -38,8 +38,8 @@ classifier = bentoml.pytorch.get('classifier:latest').to_runner()
 svc = bentoml.Service('classifier', runners=[classifier])
 
 
-@svc.api(input=Text(), output=Text())
-def inference(text: str) -> str:
+@svc.api(input=Text(), output=JSON())
+def inference(text: str) -> dict:
     """
       Perform inference on input text to predict the customer's intent.
       Args:
@@ -60,9 +60,45 @@ def inference(text: str) -> str:
         confidence_score = float(probas[0][pred_index])
         predicted_intent = label_mapping[pred_index]
         
-        log_query_to_db(text, predicted_intent, confidence_score)
+        query_id = log_query_to_db(text, predicted_intent, confidence_score)
+        # print("Query_id", query_id)
         
         print(f"Predicted_labe:, {predicted_intent}", 
               f"confidence_score: {confidence_score}")
         
-        return predicted_intent
+        return {
+            "predicted_intent": predicted_intent,
+            "confidence_score": confidence_score,
+            "query_id": query_id
+        }
+      
+
+@svc.api(input=JSON(), output=JSON())
+def submit_feedback(feedback_data: dict) -> dict:
+    """
+    Submit feedback about the prediction.
+
+    Args:
+        feedback_data (dict): A dictionary containing feedback information. 
+        Expected keys:
+            - query_id (int): The ID of the user query.
+            - is_correct (bool): Whether the prediction was correct.
+            - corrected_intent (str, optional): The corrected intent if the 
+                                                prediction was incorrect.
+    Returns:
+        dict: A confirmation message.
+    """
+    query_id = feedback_data.get("query_id")
+    is_correct = feedback_data.get("is_correct")
+    corrected_intent = feedback_data.get("corrected_intent", None)
+
+    if not query_id:
+        return {"error": "Query ID is missing."}, 400
+    if is_correct is None:
+        return {"error": "is_correct field is required"}, 400
+
+    try:
+        log_feedback_to_db(query_id, is_correct, corrected_intent)
+        return {"message": "Feedback submitted successfully"}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
